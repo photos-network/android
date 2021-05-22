3b77599e790a3bce82001e6fe5e79fe839e37c52
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2021-05-22 08:42:04 +0200

    use settings for baseurl

--- app/src/main/java/network/photos/android/DependencyInjectionModule.kt
+++ app/src/main/java/network/photos/android/DependencyInjectionModule.kt
@@ -7,6 +7,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import network.photos.android.data.photos.network.PhotoApi
+import network.photos.android.data.photos.repository.PhotoRepository
+import network.photos.android.data.photos.repository.PhotoRepositoryImpl
 import network.photos.android.data.settings.repository.SettingsRepository
 import network.photos.android.data.settings.repository.SettingsRepositoryImpl
 import network.photos.android.data.settings.storage.SettingsStorage
@@ -25,24 +28,41 @@ import java.io.File
 @InstallIn(SingletonComponent::class)
 object DependencyInjectionModule {
     @Provides
-    fun providesOkHttp(@ApplicationContext context: Context) = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().also { it.setLevel(HttpLoggingInterceptor.Level.BODY) })
-        .cache(Cache(File(context.cacheDir, "http_cache"), 20 * 1024 * 1024))
-        .build()
+    fun providesOkHttp(
+        @ApplicationContext context: Context
+    ): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().also { it.setLevel(HttpLoggingInterceptor.Level.BODY) }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .cache(Cache(File(context.cacheDir, "http_cache"), 20 * 1024 * 1024))
+            .build()
+    }
 
     @Provides
-    fun providesRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit = Retrofit.Builder()
-        .client(okHttpClient)
-        .baseUrl("https://test.photos.network")
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .build()
+    fun providesRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi,
+        settingsRepository: SettingsRepository
+    ): Retrofit {
+        val settings = settingsRepository.loadSettings()
+        val host = settings?.host ?: "https://photos.network"
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(host)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
 
     @Provides
-    fun providersMoshi(): Moshi = Moshi.Builder()
-        .build()
+    fun providersMoshi(): Moshi = Moshi.Builder().build()
 
     @Provides
     fun providesUserApi(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
+
+    @Provides
+    fun providesPhotoApi(retrofit: Retrofit): PhotoApi = retrofit.create(PhotoApi::class.java)
 
     @Provides
     fun provideUserRepository(
@@ -52,6 +72,16 @@ object DependencyInjectionModule {
         return UserRepositoryImpl(
             userApi = userApi,
             userStorage = UserStorage(context)
+        )
+    }
+
+    @Provides
+    fun providePhotoRepository(
+        @ApplicationContext context: Context,
+        photoApi: PhotoApi
+    ): PhotoRepository {
+        return PhotoRepositoryImpl(
+            photoApi = photoApi
         )
     }
 
---
+++ app/src/main/java/network/photos/android/data/photos/network/PhotoApi.kt
@@ -0,0 +1,29 @@
+package network.photos.android.data.photos.network
+
+import network.photos.android.data.photos.domain.PhotoElement
+import network.photos.android.data.photos.network.model.PhotosResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface PhotoApi {
+
+    /**
+     * List all photos for current User has access to
+     */
+    @Headers("Accept: application/json")
+    @GET("v1/photos")
+    suspend fun getPhotos(
+        @Query("offset") offset: Int = 0,
+        @Query("limit") limit: Int = 50
+    ): PhotosResponse
+
+    /**
+     * Load detailed information for a single photo
+     *
+     * @param photoId Identifier of the photo details to return
+     */
+    @Headers("Accept: application/json")
+    @GET("v1/photo/{photoId}")
+    suspend fun getPhoto(photoId: String): PhotoElement
+}
