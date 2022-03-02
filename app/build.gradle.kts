0637eb969737f3021d099fcb3d6943801b3649a1
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2022-03-02 15:48:08 +0100

    setup CI/CD with release key within github secrets (#68)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,12 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
 }
 
+// key.properties
+val releaseKeystore: String by project
+val releaseStorePassword: String by project
+val releaseKeyAlias: String by project
+val releaseKeyPassword: String by project
+
 repositories {
     google()
     mavenCentral()
@@ -88,16 +94,24 @@ android {
             keyAlias = "android"
             keyPassword = "android"
         }
+        create("release") {
+            storeFile = file(path = releaseKeystore)
+            storePassword = releaseStorePassword
+            keyAlias = releaseKeyAlias
+            keyPassword = releaseKeyPassword
+        }
     }
 
     buildTypes {
         debug {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
         release {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
