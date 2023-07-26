85655156af2520c3e66eaa3387d54916bbc9e40c
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2023-07-26 21:59:44 +0200

    spotless

--- ui/settings/src/test/kotlin/photos/network/ui/settings/composable/SettingsHeader.kt
+++ ui/settings/src/test/kotlin/photos/network/ui/settings/composable/SettingsHeader.kt
@@ -1,11 +1,26 @@
+/*
+ * Copyright 2020-2023 Photos.network developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package photos.network.ui.settings.composable
 
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.InstantAnimationsRule
 import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams
 import org.junit.Rule
 import org.junit.Test
-import com.android.ide.common.rendering.api.SessionParams
 import photos.network.api.ServerStatus
 import photos.network.ui.common.theme.AppTheme
 
@@ -18,7 +33,7 @@ class SettingsHeader {
         deviceConfig = DeviceConfig.PIXEL_5,
         showSystemUi = false,
         supportsRtl = true,
-        renderingMode = SessionParams.RenderingMode.SHRINK
+        renderingMode = SessionParams.RenderingMode.SHRINK,
     )
 
     @Test
